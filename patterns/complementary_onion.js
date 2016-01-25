28c73801b5af2c36a799fce4aaab6b699a9fd103
Gabe Conradi <gabe.conradi@gmail.com>
2016-01-24 22:15:32 -0500

    fix onion

--- patterns/complementary_onion.js
+++ patterns/complementary_onion.js
@@ -1,5 +1,5 @@
 /**
-  Wrap orb in color, then move to complement with interpolation
+  Split orb in 50/50, interpolate between complement colors as it rotates
 **/
 var OPC = require('../opc');
 var chromath = require('chromath');
@@ -13,14 +13,12 @@ function betweenwin(x,start,stop){
 }
 
 module.exports = function draw(model, client, data) {
-    var spinmod = (Math.sin(Date.now()/data.period)+1.0)/2.0;
     var c1 = chromath.rgb( {r:data.red,g:data.green,b:data.blue});
     var c2 = chromath.complement(c1);
 
     client.mapPixels(function(led){
       var x = led.point[0];
-      var p = (Math.sin(x+Date.now()/data.period)+1.0)/2.0;
-      //var p = Math.sin(x+Date.now()/data.period);
-      return chromath.towards(c2, c1, p).toRGBArray();
+      var p = (Math.sin(Math.abs(x)+Date.now()/data.period*10.0)+1.0)/2.0;
+      return chromath.towards(c1, c2, p).toRGBArray();
     }, model);
 };
