e46f8938e43952cfe5827ee6f3e9caa692666a5c
Gabe Conradi <gabe.conradi@gmail.com>
2016-01-24 23:15:36 -0500

    complementary split with fades

---
+++ patterns/complementary_split.js
@@ -0,0 +1,14 @@
+var OPC = require('../opc');
+var chromath = require('chromath');
+
+module.exports = function draw(model, client, data) {
+    var c1 = chromath.rgb( {r:data.red,g:data.green,b:data.blue});
+    var c2 = chromath.complement(c1);
+
+    var dp = (Date.now()%(data.period/2.0))/(data.period/2.0);
+    client.mapPixels(function(led){
+      var x = led.point[0];
+      var wrapx = (x+1.0+dp*2.0)%2.0 - 1.0;
+      return chromath.towards(c1, c2, Math.abs(wrapx)).toRGBArray();
+    }, model);
+};
