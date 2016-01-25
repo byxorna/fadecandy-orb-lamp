df6dee19477a683563915334fe69e94a8eeab63b
Gabe Conradi <gabe.conradi@gmail.com>
2016-01-25 00:14:15 -0500

    update particle trails

--- patterns/particle_trails_1.js
+++ patterns/particle_trails_1.js
@@ -1,38 +1,31 @@
 // this is aped from particle_trail.js: https://github.com/scanlime/fadecandy/blob/master/examples/node/particle_trail.js
 
 var OPC = require('../opc');
+var chromath = require('chromath');
 
-/*TODO
-// keep the points between -1.0:1.0 x, -0.5:0.5 z
-var bounds = {
-  maxX: 1.0,
-  minX: -1.0,
-  maxY: 0.5,
-  minY: -0.5,
-};
-*/
 module.exports = function draw(model, client, data) {
 
+    var dt = (Date.now()%data.period)/data.period;
     var time = 0.001 * new Date().getTime();
     var numParticles = 100;
-    //var numParticles = 200;
     var particles = [];
 
     for (var i = 0; i < numParticles; i++) {
         var s = i / numParticles;
-        var radius = 0.2 + 1.5 * s;
+        //var radius = 0.2 + 1.5 * s;
         var theta = time + 0.04 * i;
-        var x = radius * Math.cos(theta);
-        var y = radius * Math.sin(theta + 10.0 * Math.sin(theta * 0.15));
-        var hue = time * 0.01 + s * 0.2;
-        //var scaledPoints = scalePoints(x,y);
+        //var x = radius * Math.cos(theta);
+        //var y = radius * Math.sin(theta + 10.0 * Math.sin(theta * 0.15));
+        var x = Math.cos(theta);
+        var y = 0.5* Math.sin(theta + 10.0 * Math.sin(theta * 0.15));
+        //var hue = time * 0.01 + s * 0.2;
+        var c = chromath.hsv(dt*360,1,1).toRGBArray();
         particles[i] = {
-            //point: [scaledPoints.x, 0, scaledPoints.y],
             point: [x, 0, y],
             intensity: 0.2 * s,
             falloff: 60,
             //color: OPC.hsv(hue, 0.5, 0.8)
-            color: OPC.hsv(hue, 1.0,1.0)
+            color: c,
         };
     }
 
