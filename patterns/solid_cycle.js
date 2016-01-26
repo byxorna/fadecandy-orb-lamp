3851ac851ba1ac9663ea0e75fcc451a21fdfdf2e
Gabe Conradi <gabe.conradi@gmail.com>
2016-01-25 22:19:36 -0500

    turn down speed on solid cycle

--- patterns/solid_cycle.js
+++ patterns/solid_cycle.js
@@ -5,7 +5,8 @@ var OPC = require('../opc');
 var chromath = require('chromath');
 
 module.exports = function draw(model, client, data) {
-    var hue_progression = (Date.now() % data.period)/data.period;
+    var dp = data.period * 4;
+    var hue_progression = (Date.now() % dp)/dp;
     var c = chromath.hsv(360*hue_progression,1,1).toRGBArray();
     //console.log("hue progression:",hue_progression, c);
     client.mapPixels(function(_){ return c; }, model);
