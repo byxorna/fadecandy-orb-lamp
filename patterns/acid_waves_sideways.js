06ab2910e8bfa09f44afa8c3e5ed2b293b709463
Gabe Conradi <gabe.conradi@gmail.com>
2016-01-25 22:15:48 -0500

    acid waves

---
+++ patterns/acid_waves_sideways.js
@@ -0,0 +1,12 @@
+var OPC = require('../opc');
+var chromath = require('chromath');
+
+module.exports = function draw(model, client, data) {
+    var dt = (Date.now()%data.period)/data.period;
+    client.mapPixels(function(led){
+      var x = led.point[0];
+      var v = ((x+1.0+2*dt)%2.0)/2.0;
+      var c = chromath.hsv(360*v,1,1);
+      return c.toRGBArray();
+    }, model);
+};
---
+++ patterns/acid_waves_wrap_unwrap.js
@@ -0,0 +1,12 @@
+var OPC = require('../opc');
+var chromath = require('chromath');
+
+module.exports = function draw(model, client, data) {
+    var dt = Math.sin(Date.now()/data.period);
+    client.mapPixels(function(led){
+      var x = led.point[0];
+      var v = Math.sin((x+1+dt*2*Math.PI)*Math.PI/4);
+      var c = chromath.hsv(360*v,1,1);
+      return c.toRGBArray();
+    }, model);
+};
