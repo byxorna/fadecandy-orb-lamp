370b96998b84a0e22f963732581b29cede7ad28e
Gabe Conradi <gabe.conradi@gmail.com>
2016-02-07 15:44:00 -0500

    tendrils

---
+++ patterns/spinning_tendrils_color.js
@@ -0,0 +1,27 @@
+var OPC = require('../opc');
+var chromath = require('chromath');
+var numParticles = 40;
+
+module.exports = function draw(model, client, data) {
+
+    //var dt = (Date.now()%data.period)/data.period;
+    var time = Date.now()/data.period*60;
+    var particles = [];
+
+    for (var i = 0; i < numParticles; i++) {
+        var s = 1; //i / numParticles;
+        var theta = (0.001*Date.now()) + 0.06 * i;
+        var x = 1.0 - theta%2.0;  // constant speed
+        var y = 0.5-i/numParticles;
+        var c = chromath.hsv((time)%360,1,1).toRGBArray();
+        particles[i] = {
+            point: [x, 0, y],
+            intensity: 0.2 * s,
+            falloff: 180,
+            color: c,
+        };
+    }
+
+    client.mapParticles(particles, model);
+};
+
---
+++ patterns/spinning_tendrils_speed.js
@@ -0,0 +1,26 @@
+var OPC = require('../opc');
+var chromath = require('chromath');
+var numParticles = 40;
+
+module.exports = function draw(model, client, data) {
+
+    var time = 0.001 * new Date().getTime();
+    var particles = [];
+    var theta = Date.now()/data.period*6;
+
+    for (var i = 0; i < numParticles; i++) {
+        var s = 1;
+        var x = 1.0 - (theta+0.06*i)%2.0;
+        var y = 0.5-i/numParticles;
+        var c = chromath.hsv((theta*0.01*360)%360,1,1).toRGBArray();
+        particles[i] = {
+            point: [x, 0, y],
+            intensity: 0.2 * s,
+            falloff: 180,
+            color: c,
+        };
+    }
+
+    client.mapParticles(particles, model);
+};
+
