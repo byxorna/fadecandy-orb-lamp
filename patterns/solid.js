0f5a0ec59980f48027d7b89086d785ad9e06a199
Gabe Conradi <gabe.conradi@gmail.com>
2016-07-02 19:34:13 -0400

    try solid darkening

--- patterns/solid.js
+++ patterns/solid.js
@@ -1,9 +1,11 @@
 var OPC = require('../opc');
+var chromath = require('chromath');
 /**
   Draws a solid color, based on the rgb values in data
 **/
 
 module.exports = function draw(model, client, data) {
-    var c = [data.red,data.green,data.blue];
+    var c = chromath.rgb(data.red,data.green,data.blue)
+      .darken(1.0-data.intensity);
     client.mapPixels(function(_){ return c; }, model);
 };
