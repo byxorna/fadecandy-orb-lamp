f56211db4ac0415fb51ee6043839ba32dfee512c
Gabe Conradi <gabe.conradi@gmail.com>
2016-11-29 20:20:30 -0500

    fix random

--- patterns/random.js
+++ patterns/random.js
@@ -1,49 +1,49 @@
-var fs = require('fs');
-var _ = require('underscore');
-var files = fs.readdirSync(__dirname);
-var PERIOD = 10000;
-var patterns = {};
+module.exports = function (){
+  var fs = require('fs');
+  var _ = require('underscore');
+  var files = fs.readdirSync(__dirname);
+  var PERIOD = 10000;
+  var patterns = {};
 
-for (var i = 0; i<files.length; i++){
-  var fName = __dirname + "/" + files[i];
-  var default_features = {
-    color: true,
-    intensity: true,
-    period: true
-  };
-  if (fName == __filename) {
-    // dont load ourselves!
-    continue;
-  }
-  if (files[i].match(/^\w+\.js$/)) {
-    var name = files[i].replace(/\.[^/.]+$/, "");
-    patterns[name] = require(fName);
-    if (typeof(patterns[name]) == 'function') {
-      // use default features
-      patterns[name] = {
-        features: default_features,
-        draw: patterns[name]
-      };
-    } else {
-      // merge in default features
-      patterns[name]['features'] = _.extend(default_features, patterns[name]['features']);
+  for (var i = 0; i<files.length; i++){
+    var fName = __dirname + "/" + files[i];
+    var default_features = {
+      color: true,
+      intensity: true,
+      period: true
+    };
+    if (fName == __filename) {
+      // dont load ourselves!
+      continue;
     }
-	}
-}
+    if (files[i].match(/^\w+\.js$/)) {
+      var name = files[i].replace(/\.[^/.]+$/, "");
+      patterns[name] = require(fName);
+      if (typeof(patterns[name]) == 'function') {
+        // use default features
+        patterns[name] = {
+          features: default_features,
+          draw: patterns[name]
+        };
+      } else {
+        // merge in default features
+        patterns[name]['features'] = _.extend(default_features, patterns[name]['features']);
+      }
+    }
+  }
 
-function pickRandomPattern(){
-  var patternsKeys = Object.keys(patterns);
-  Math.round(Math.random()*patternsKeys.length);
-  var patternName = patternsKeys[Math.floor(Math.random()*patternsKeys.length)];
-  var activePattern = patterns[patternName]['draw'];
-};
+  function pickRandomPattern(){
+    var patternsKeys = Object.keys(patterns);
+    Math.round(Math.random()*patternsKeys.length);
+    var patternName = patternsKeys[Math.floor(Math.random()*patternsKeys.length)];
+    var p = patterns[patternName];
+    console.log("random: setting pattern to " + patternName);
+    activePattern = patterns[patternName]['draw'];
+  };
 
-var activePattern = pickRandomPattern();
-setInterval(function() {
-  activePattern = pickRandomPattern();
-}, PERIOD);
+  var activePattern = null;
+  pickRandomPattern();
+  setInterval(pickRandomPattern, PERIOD);
 
-module.exports = function draw(model, client, data) {
-  return activePattern(model,client,data);
-}
-
+  return function(model, client, data) { return activePattern(model,client,data); };
+}();
